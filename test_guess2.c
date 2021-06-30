1c5204f700b57990094258fa25669006fbc3bb30
overlord <506421403@qq.com>
2021-07-01 01:22:48 +0800

    macro compatibility test.

--- test_guess2.c
+++ test_guess2.c
@@ -3,15 +3,47 @@
 //
 
 #include <stdio.h>
-#include <xthreads.h>
+#include <stdlib.h>
 
-int main() {
-    puts("23123\n");
 #ifdef _WINDOWS
-    puts("macro defined WINDOWS.\n");
+
+#include <thr/xthreads.h>
+
+#define THREAD_HEAD(TAILNAME) _Thrd_ ## TAILNAME
+#define THREAD_CURRENT _Thrd_current()._Id
 #else
-    puts("macro not defined WINDOWS.\n");
+
+#include <threads.h>
+
+#define THREAD_HEAD(TAILNAME) thrd_ ## TAILNAME
+#define THREAD_CURRENT thrd_current()
 #endif
 
-    thrd_create()
+#define THREADSNUM 4
+
+int thrd_func(void *args);
+
+int main() {
+    THREAD_HEAD(t) threads[THREADSNUM];
+    char *trash[THREADSNUM];
+
+    for (int i = 0; i < THREADSNUM; ++i) {
+        trash[i] = malloc(20);
+        sprintf(trash[i], "It's %d thread.", i + 1);
+        THREAD_HEAD(create)(&threads[i], thrd_func, trash[i]);
+    }
+
+    for (int i = 0; i < THREADSNUM; ++i) {
+        int ret = THREAD_HEAD(join)(threads[i], NULL);
+        free(trash[i]);
+    }
+
+    return 0;
+}
+
+int thrd_func(void *args) {
+    char *str = (char *) args;
+    printf("Thread %u deliver message: %s\n", THREAD_CURRENT, str);
+//    _Thrd_current()._Id
+    return THREAD_HEAD(success);
 }
